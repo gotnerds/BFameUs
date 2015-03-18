e4db4f6247d61c2b6ee16c7ab3a6e2fa1823ed2f
sreejus <sreeju.php@gmail.com>
2015-03-18 19:34:23 +0530

    Added dummy categories, tested file upload, added hot videos section, added top 10 videos section, added favorites videos section

--- toprated.php
+++ toprated.php
@@ -16,7 +16,7 @@ $tag_cloud 		= make_tag_cloud('videos');
 $tag_cloud_block	= $tag_cloud[1];
 
 //get all categories
-$all_categories	= array();
+$top_rated_videos	= array();
 $count_categories	= array();
 //get all categories
 $all_categories	= array();
@@ -41,50 +41,36 @@ while ($result = @mysql_fetch_array($query)) {
 
 }
 
-
-$limit 		= (int) mysql_real_escape_string($config['see_more_limits']);
-$pagination = pagination("SELECT channel_name, channel_name_seo, channel_id FROM channels ORDER BY channel_name ASC", $limit);
-$set_limit = $pagination[0]['set_limit'];
-$total_pages = $pagination[0]['total_pages'];
-$current_page = $pagination[0]['current_page'];
-$total_records = $pagination[0]['total_records'];
-$next_page = $pagination[0]['next_page'];//use in html navigation (src)
-$prev_page = $pagination[0]['prev_page'];//use in html navigation (src)
-$nl = $pagination[0]['nl'];//use in html navigation: next>>
-$pl = $pagination[0]['pl'];//use in html navigation: <<previous
-
-$sql = "SELECT channel_name, channel_name_seo, channel_id FROM channels ORDER BY channel_name ASC LIMIT $set_limit, $limit";
+$sql = "SELECT * FROM videos WHERE approved='yes' $sql_public_private ORDER BY rating_total_points DESC LIMIT 0, 10";
 $query 	= @mysql_query($sql);
 while ($result = @mysql_fetch_array($query)) {
-	$channel_id	= $result['channel_id'];
-    $sql_query 		= "SELECT video_id FROM videos WHERE channel_id = '$channel_id' AND approved = 'yes' $sql_public_private ORDER BY indexer DESC LIMIT 0,1";
-    $query_data 		= @mysql_query($sql_query);    
-    
-    $video_data = @mysql_fetch_array($query_data);
+		//get comments inforation
+        $video_id = mysql_real_escape_string($result['indexer']);
+        $sql2 = "SELECT * FROM videocomments WHERE video_id = $video_id";
+        $query2 = @mysql_query($sql2);
+        $comments_number = @mysql_num_rows($query2);
+        $comments_array = array('comments' => $comments_number);
 
-    $new_array	= @array_merge($result, $video_data);
-    
-    $all_categories[] = $new_array;
+        //get star rating
+        $stars_array = stars_array($video_id);//call the stars function (results returned as array)
+
+        //merge comments array
+        $result2 = @array_merge($result, $comments_array, $stars_array);
+        $top_rated_videos[] = $result2;
   
 }
 
-$url = 'genres.php';								
-$additional_url_variable = '?page=';
-
-@include_once ($include_base . '/includes/pagination.inc.php');
-
-//echo "<pre>";print_r($all_categories);exit;
 
 //display results
 
 $template		= "themes/$user_theme/templates/main_1.htm";
-$inner_template1	= "themes/$user_theme/templates/inner_generes.htm";
+$inner_template1	= "themes/$user_theme/templates/inner_toprated.htm";
 $TBS			= new clsTinyButStrong;
 $TBS->NoErr 	= true;
 
 $TBS->LoadTemplate("$template");
 
-$TBS->MergeBlock('all_categories', $all_categories);
+$TBS->MergeBlock('top_rated_videos', $top_rated_videos);
 $TBS->MergeBlock('blk1', $count_categories);
 
 $TBS->Render 	= TBS_OUTPUT;
